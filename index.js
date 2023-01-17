36de910ea8621fa4278a8b682347173ba52d6b4f
Soumitra Lahiri <soumitra.lahiri@gmail.com>
2023-01-17 01:43:23 -0800

    Initial commit of Backend

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+import authRoutes from './routes/auth.js';
+
+dotenv.config();
+
+const app = express();
+
+// db
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('DB Connected'))
+  .catch((err) => console.log('DB ERROR => ', err));
+
+// middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+// router middleware
+app.use('/api', authRoutes);
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Node server is running on port ${port}`);
+});
