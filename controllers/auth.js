5b91a17a518a8d637bf10cd3c200c60b003f797f
Soumitra Lahiri <soumitra.lahiri@gmail.com>
2023-01-17 10:44:03 -0800

    Implemented JWT Token generation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,9 @@
 import User from '../models/user.js';
 import { hashPassword, comparePassword } from '../helpers/auth.js';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const register = async (req, res) => {
   try {
@@ -28,8 +32,60 @@ export const register = async (req, res) => {
       email,
       password: hashedPassword,
     }).save();
-    // 6. send response
-    res.json(user);
+    // 6. create signed jwt
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '7d',
+    });
+    // 7. send response
+    res.json({
+      user: {
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        address: user.address,
+      },
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    // 1. destructure name, email, password from req.body
+    const { email, password } = req.body;
+    // 2. all fields require validation
+    if (!email) {
+      return res.json({ error: 'Email is required' });
+    }
+    if (!password || password.length < 6) {
+      return res.json({ error: 'Password must be at least 6 characters long' });
+    }
+    // 3. check if email is taken
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.json({ error: 'User not found' });
+    }
+    // 4. compare password
+    const match = await comparePassword(password, user.password);
+    if (!match) {
+      return res.json({ error: 'Wrong password' });
+    }
+    // 5. create signed jwt
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '7d',
+    });
+    // 7. send response
+    res.json({
+      user: {
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        address: user.address,
+      },
+      token,
+    });
   } catch (err) {
     console.log(err);
   }
